989a1897695a9e8fd9537e1f652067118ba7be78
Damian <damianmdst@gmail.com>
2021-07-03 21:27:32 -0300

    Create repository, configure eslint, prettier, husky, tailwindo and lint-staged

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Header</h1>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'App',
+    components: {},
+  }
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { join } = require('path')
+const BASE_DIR = join(__dirname, 'src')
+const VUE_FILE = join('**', '*.vue')
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [join(BASE_DIR, VUE_FILE), join(__dirname, '*.html')],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
